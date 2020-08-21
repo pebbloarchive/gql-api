b434c787bf825477e7111ebf4151c4ed8f661f42
unknown <k00benne@gmail.com>
2020-08-21 13:09:10 -0400

    Coming soon

--- src/routers/v1/admin.ts
+++ src/routers/v1/admin.ts
@@ -1,14 +1,26 @@
 import { Router, Response, Request, NextFunction } from 'express';
 import session from '../../middleware/session';
+import { MessageChannel } from 'worker_threads';
 
 const router = Router();
 
 router.post('/ban/:id', session, async (req: Request, res: Response, next: NextFunction) => {
+  const { time } = req.body;
   // @ts-ignore
-  if(!req.user.permissions.includes(/(admin|developer|staff)/))
-    return res.status(401).send({ erorr: 'Unable to use this endpoint due to missing permissions' }); 
+  const me = await db.users.findOne({ id: req.user.id });
+  // @ts-ignore
+  if(!me.permissions.includes(/(admin|developer|staff)/))
+    return res.status(401).send({ error: 'Unable to use this endpoint due to missing permissions' }); 
 
-  return res.status(200).send({ ok: true });
+  try {
+    // @ts-ignore
+    const user = await db.users.findOne({ id: req.params.id });
+    if(!user) return res.status(404).send({ error: 'Unable to find that user' });
+    // @ts-ignore
+    await db.users.updateOne({ id: req.params.id }, { suspended: true });
+  } catch(err) {
+    return res.send(400).send({ error: 'It seems something went wrong' });
+  }
 });
 
 export default router;

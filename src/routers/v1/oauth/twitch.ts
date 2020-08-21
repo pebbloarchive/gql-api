a8c78eb15139ca8d219016b96ea6333c88ceff5e
unknown <k00benne@gmail.com>
2020-08-21 13:12:24 -0400

    just yeet everything

--- src/routers/v1/oauth/twitch.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Response, Request } from 'express';
-
-const router = Router();
-
-router.get('/', async (req: Request, res: Response) => {
-  return res.send('hello world');
-});
-
-export default router;

1e14304870d94606a9a3247b74b52f081d4d1cd2
unknown <k00benne@gmail.com>
2020-08-25 00:32:46 -0400

    Message reworks

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,33 @@ export const BLACKLISTED_USERNAMES = /(pebblo|create|signup|register|login|creat
 export const SPOTIFY_URL = 'https://accounts.spotify.com/api/token';
 export const SPOTIFY_API = 'https://api.spotify.com/v1/me';
 export const SPOTIFY_REDIRECT = 'http://localhost:3000/1.0/auth/spotify/callback';
+export const RANDOM_STRING = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+// MESSAGES
+
+export const COMMON_ERROR = 'It seems something went wrong, please try again.';
+export const INVALID_PASSWORD = 'Unable to login, an invalid password was given.';
+export const INVALID_INFO = 'Missing required information for request.';
+export const INVALID_USERNAME = 'Usernames can\'t be shorter than one character, or longer than 25 characters.';
+export const UNSUPPORTED_USERNAME = 'Usernames can\'t include unicode.';
+export const INVALID_EMAIL = 'An invalid email was provided.';
+export const WEAK_PASSWORD = 'Your password was too weak, please choose a stronger password.';
+export const INVALID_CONF_PASSWORD = 'Your passwords do not match each other.';
+export const USERNAME_TAKEN = 'Sorry, that username is already in use.';
+export const EMAIL_TAKEN = 'Sorry, that email is already in use.';
+export const INVALID_USER = 'Unable to find that user.';
+export const INVALID_EMAIL_CODE = 'The verification code entered was invalid.';
+export const INVALID_MFA_CODE = 'The mfa code entered was invalid.';
+export const INVALID_JWT = 'Invalid authorization data was passed.';
+export const EXPIRED_JWT = 'Expired authorization token was passed.';
+
+// FUNCTIONS
+
+export function genToken(length: number) {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}

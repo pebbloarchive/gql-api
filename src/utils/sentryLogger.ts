a2b81e5fefb587249ac823afd217b48ac6f96f8f
Kyle <k00benne@gmail.com>
2020-12-06 18:15:12 -0500

    Update gitlab YML

--- src/utils/sentryLogger.ts
+++ src/utils/sentryLogger.ts
@@ -1,15 +1,15 @@
-import * as Sentry from '@sentry/node';
+import * as Sentry from "@sentry/node";
 
 Sentry.init({
-    dsn: process.env.SENTRY_DSN
+  dsn: process.env.SENTRY_DSN,
 });
 
 export const captureException = Sentry.captureException;
 
 export const Logger = async (err) => {
-    console.error({ err });
-    Sentry.captureException(err);
-    return await Sentry.flush(2000);
-}
+  console.error({ err });
+  Sentry.captureException(err);
+  return await Sentry.flush(2000);
+};
 
 export default Logger;

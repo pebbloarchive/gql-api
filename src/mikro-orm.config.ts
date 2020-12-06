49f18f858445fad40f880ce4e8f8a5a9c949ef31
Kyle <k00benne@gmail.com>
2020-12-06 04:16:21 -0500

    Update

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { MikroORM } from "@mikro-orm/core";
 import { Post } from "./entites/Post";
 import { User } from "./entites/user";
 import path from "path";
+import { IS_DEV } from "./constants";
 
 export default {
         migrations: {
@@ -9,9 +10,6 @@ export default {
                 pattern: /^[\w-]+\d+\.[tj]s$/,
         }, 
         entities: [Post, User],
-        dbName: 'pebblo',
-        type: 'postgresql',
-        user: 'postgres',
-        password: 'djshawn1',
-        debug: true
+        clientUrl: process.env.DATABASE_URL,
+        debug: IS_DEV ? true : false
 } as Parameters<typeof MikroORM.init>[0];
